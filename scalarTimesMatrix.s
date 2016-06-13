7454b00d6d26d56421f5b7aed2487088e258ea03
ottozinho <wesley.otto@hotmail.com>
2016-06-13 15:08:07 -0300

    [wip]
    git commit -m [wip]

---
+++ scalarTimesMatrix.s
@@ -0,0 +1,31 @@
+; Assembly Intel
+  SECTION .text
+
+  global scalarTimesMatrix_s
+
+scalarTimesMatrix_s:
+  push %ebp
+  mov %esp, %ebp
+
+  mov 8(%ebp),  %ecx  ; L
+  mov 12(%ebp), %ebx  ; scalar
+  mov 16(%ebp), %esi, ; matrix 1st element (base)
+  mov 20(%ebp), %edi  ; outputMatrix 1st element
+
+  xor %eax, %eax
+  mov %eax, %ecx
+  mul %ecx
+  mov %ecx, %eax ; ecx = L*L (size of the matrix)
+  dec %ecx      ; (L*L)-1   (Matrix's last element)
+
+loop:
+    ; counter ecx (L*L)-1 ~ 0
+    mov (%esi + 4(%ecx)), %eax ; iterating multiples of 4
+    mul %bx                     ; scalar * ax
+    mov %eax, (%edi + 4(%ecx))  ; set the correspondent outputMatrix
+    dec ecx
+jns loop  ; loop L*L times - (do while)
+
+  mov %ebp, %esp
+  pop %ebp
+  ret
