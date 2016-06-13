8392184e68e529bb43fb93f8b30cd9d567d18425
daltonbr <sacinopatinete@gmail.com>
2016-06-13 16:47:36 -0300

    Finish greaterInDiagonal.s, Add mainATT.c

--- greaterInDiagonal.s
+++ greaterInDiagonal.s
@@ -1,7 +1,8 @@
-; Assembly Intel
-          .text
+/* Assembly AT&T */
 
-  global greaterInDiagonal_s
+  .text
+
+  .global greaterInDiagonal_s
 
 greaterInDiagonal_s:
   push %ebp
@@ -11,31 +12,31 @@ greaterInDiagonal_s:
   xor %edx, %edx
   xor %eax, %eax
   xor %ecx, %ecx
-  mov 8(%ebp), %ecx   ;L
+  mov 8(%ebp), %ecx   /* L */
 
 
   mov 8(%ebp), %edx
   mov $1,%eax
-  add %ecx, %eax     ; ax = L+1
+  add %ecx, %eax     /* ax = L+1 */
   add %eax, %eax
-  add %eax, %eax     ; eax = 4*(L+1) , int = 4 bytes
+  add %eax, %eax     /* eax = 4*(L+1) , int = 4 bytes */
 
-  dec %ecx           ; L-1
-  mov %eax, %esi     ; reference to 4*(L+1)
+  dec %ecx           /* L-1 */
+  mov %eax, %esi     /* reference to 4*(L+1) */
 
-  mov 12(%ebp),%ebx  ; adress of matrix's first element
+  mov 12(%ebp),%ebx  /* adress of matrix's first element */
   xor %edi, %edi
 
 loop:
-    ;  counter ecx L-1 ~ 0
+    /*  counter ecx L-1 ~ 0  */
     mov %esi,%eax
     mul %ecx
-    cmp %edi, (%ebx + %eax)    ; compare current max value
-    ja continue           ; if not, doesn't change the current max value
-    mov (%ebx+eax),%edi    ; else...change it
+    cmp (%ebx,%eax), %edi    /* compare current max value */
+    ja continue                /* if not, doesn't change the current max value */
+    mov (%ebx,%eax), %edi    	   /* else...change it */
     continue:
     dec %ecx
-jns loop  ; loop l times
+jns loop  /* loop l times */
 
   mov %edi,%eax
   mov %ebp,%esp
